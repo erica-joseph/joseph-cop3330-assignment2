8bd3a87f97752833a1d639d3f2a2289cb2e22579
erica-joseph <ericagjoseph@outlook.com>
2021-06-13 20:08:19 -0400

    31-32

--- src/main/java/oop/assignment2/ex38/solution38.java
+++ src/main/java/oop/assignment2/ex38/solution38.java
@@ -1,4 +1,20 @@
 package oop.assignment2.ex38;
+import java.util.Scanner;
 
 public class solution38 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int input = sc.nextInt();
+
+        char[] chars = ("" + input).toCharArray();
+        System.out.println(chars);
+
+        String alphabet = "abcdefghijklmnopqrstuvwxyz";
+        if(input%2 == 0){
+            System.out.print("This is an even nubmer");
+        }
+        else{
+            System.out.print("This is an odd number");
+        }
+    }
 }
